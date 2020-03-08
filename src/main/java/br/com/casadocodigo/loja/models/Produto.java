b353e99774324e0ffe2edbe9763ea6cc093bf113
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-08 00:09:28 -0300

    configurando JPA e criando ProdutoDAO, salvando primeiro produto

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,6 +1,16 @@
 package br.com.casadocodigo.loja.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "produto")
 public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	private String titulo;
 	private String descricao;
@@ -25,6 +35,12 @@ public class Produto {
 		this.paginas = paginas;
 	}
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
