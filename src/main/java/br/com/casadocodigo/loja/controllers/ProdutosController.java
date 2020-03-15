cb9ee249dde7f88ac38d1493fb388c9547af1385
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-14 22:35:31 -0300

    retornando produto em json

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -78,6 +79,12 @@ public class ProdutosController {
 		Produto produto = produtoDAO.findById(id);
 		modelAndView.addObject("produto", produto);
 		return modelAndView;
+	}
+	
+	@RequestMapping("/{id}")
+	@ResponseBody
+	public Produto detalheJson(@PathVariable("id") Long id) {
+		return produtoDAO.findById(id);
 	}
 
 }
