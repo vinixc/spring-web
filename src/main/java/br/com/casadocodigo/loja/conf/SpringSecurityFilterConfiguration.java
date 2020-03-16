daa34581ff176d833e97f6a6ab0f250d49977bf4
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-16 20:36:01 -0300

    criando usuario e usuarioDAO, configurando usuario e role

--- src/main/java/br/com/casadocodigo/loja/conf/SpringSecurityFilterConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SpringSecurityFilterConfiguration.java
@@ -4,6 +4,4 @@ import org.springframework.security.web.context.AbstractSecurityWebApplicationIn
 
 public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer{
 	
-	
-
 }
