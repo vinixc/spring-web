14d1a5424d2fbe2d7610965626b014ee59f84266
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-17 22:34:27 -0300

    trabalhando com profiles

--- src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -3,8 +3,10 @@ package br.com.casadocodigo.loja.conf;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -16,30 +18,35 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class JPAConfiguration {
 	
 	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
 		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setPackagesToScan("br.com.casadocodigo.loja.models");
+		factoryBean.setDataSource(dataSource);		
 		
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		factoryBean.setJpaVendorAdapter(vendorAdapter);
+		factoryBean.setJpaProperties(aditionalProperties());
 		
+		return factoryBean;
+	}
+
+	private Properties aditionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		return properties;
+	}
+
+	@Bean
+	@Profile("dev")
+	private DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setUsername("vinicius");
 		dataSource.setPassword("Vini@12345678");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		
-		factoryBean.setDataSource(dataSource);
-		
-		Properties properties = new Properties();
-		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		properties.setProperty("hibernate.show_sql", "true");
-		properties.setProperty("hibernate.hbm2ddl.auto", "update");
-		
-		factoryBean.setJpaProperties(properties);
-		
-		factoryBean.setPackagesToScan("br.com.casadocodigo.loja.models");
-		
-		return factoryBean;
+		return dataSource;
 	}
 	
 	@Bean
--- src/test/java/br/com/casadocodigo/loja/daos/ProdutoDAOTest.java
+++ src/test/java/br/com/casadocodigo/loja/daos/ProdutoDAOTest.java
@@ -7,17 +7,20 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.casadocodigo.loja.builders.ProdutoBuilder;
+import br.com.casadocodigo.loja.conf.DataSourceConfigurationTest;
 import br.com.casadocodigo.loja.conf.JPAConfiguration;
 import br.com.casadocodigo.loja.enums.TipoPreco;
 import br.com.casadocodigo.loja.models.Produto;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {JPAConfiguration.class, ProdutoDAO.class})
+@ContextConfiguration(classes = {JPAConfiguration.class, ProdutoDAO.class, DataSourceConfigurationTest.class})
+@ActiveProfiles("test")
 public class ProdutoDAOTest {
 	
 	@Autowired
@@ -29,14 +32,18 @@ public class ProdutoDAOTest {
 		
 		List<Produto> livrosImpressos = ProdutoBuilder.newProduto(TipoPreco.IMPRESSO, BigDecimal.TEN).more(3).buildAll();
 		
-		List<Produto> livrosEbooks = ProdutoBuilder.newProduto(TipoPreco.EBOOK, BigDecimal.TEN).more(3).buildAll();
+		List<Produto> livrosEbooks = ProdutoBuilder.newProduto(TipoPreco.EBOOK, BigDecimal.TEN).more(4).buildAll();
 		
 		livrosImpressos.stream().forEach(produtoDAO::gravar);
 		livrosEbooks.stream().forEach(produtoDAO::gravar);
 		
 		
 		BigDecimal valor = produtoDAO.somaPrecosPorTipo(TipoPreco.EBOOK);
-		Assert.assertEquals(new BigDecimal(40).setScale(2), valor);
+		BigDecimal valorImpresso = produtoDAO.somaPrecosPorTipo(TipoPreco.IMPRESSO);
+
+		
+		Assert.assertEquals(new BigDecimal(50).setScale(2), valor);
+		Assert.assertEquals(new BigDecimal(40).setScale(2), valorImpresso);
 	}
 
 }
