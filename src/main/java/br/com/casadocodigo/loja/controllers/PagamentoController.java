c61ade09d694e7d8b7390259a9e9aa01f017aee0
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-17 23:54:22 -0300

    configurando envio de email

--- src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -3,6 +3,9 @@ package br.com.casadocodigo.loja.controllers;
 import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
 import br.com.casadocodigo.loja.models.DadosPagamento;
+import br.com.casadocodigo.loja.models.Usuario;
 
 @Controller
 @RequestMapping("/pagamento")
@@ -23,8 +27,11 @@ public class PagamentoController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private MailSender sender;
+	
 	@RequestMapping(value="/finalizar", method =  RequestMethod.POST)
-	public Callable<ModelAndView> finalizar(RedirectAttributes model){
+	public Callable<ModelAndView> finalizar(@AuthenticationPrincipal Usuario usuario, RedirectAttributes model){
 		return () -> {
 			
 			try {
@@ -32,6 +39,7 @@ public class PagamentoController {
 				String uri = "http://book-payment.herokuapp.com/payment";
 				String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);
 				System.out.println(response);
+				enviaEmailCompraProduto(usuario);
 				model.addFlashAttribute("sucesso", response);
 				return new ModelAndView("redirect:/produtos");
 			}catch(HttpClientErrorException e ) {
@@ -40,5 +48,15 @@ public class PagamentoController {
 				return new ModelAndView("redirect:/produtos");
 			}
 		};		
+	}
+
+	private void enviaEmailCompraProduto(Usuario usuario) {
+		SimpleMailMessage email = new SimpleMailMessage();
+		email.setSubject("Compra finalizada com sucesso");
+		email.setTo(usuario.getEmail());
+		email.setText("Compra aprovada com sucesso no valor de " + carrinho.getTotal());
+		email.setFrom("compras@sexshop.com.br");
+		
+		sender.send(email);
 	}
 }
