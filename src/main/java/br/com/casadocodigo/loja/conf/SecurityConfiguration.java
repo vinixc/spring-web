1099dfc8f8d9107bcde9c9a75bf4b37aa8a151aa
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-17 23:18:00 -0300

    varias roles para uma pagina

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -22,7 +22,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/resources/**").permitAll()
-			.antMatchers("/produtos/form").hasRole("ADMIN")
+			.antMatchers("/produtos/form").hasAnyRole("ADMIN")
 			.antMatchers("/carrinho").permitAll()
 			.antMatchers(HttpMethod.POST,"/produtos").hasRole("ADMIN")
 			.antMatchers(HttpMethod.GET, "/produtos").hasRole("ADMIN")
